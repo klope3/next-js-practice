d5dc5cd4c1def8801ac213a3e5a5eb7a47293b29
Josh Klope <klope3@msn.com>
2024-05-04 11:41:14 -0500

    Add category creation

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,4 +1,22 @@
+import { revalidatePath } from "next/cache";
 import { prisma } from "../../../prisma/client";
+import { redirect } from "next/navigation";
+
+async function createCategory(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name");
+  if (!name) return;
+
+  await prisma.category.create({
+    data: {
+      name: `${name}`,
+    },
+  });
+
+  revalidatePath("/categories");
+  redirect("/categories");
+}
 
 export default async function Categories() {
   const categories = await prisma.category.findMany();
@@ -11,6 +29,11 @@ export default async function Categories() {
           <li key={category.id}>{category.name}</li>
         ))}
       </ul>
+      <form action={createCategory}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
