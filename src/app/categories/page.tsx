63c5171718deccefd3e836a87da90c5b08c7f38c
Josh Klope <klope3@msn.com>
2024-05-05 20:00:08 -0500

    Add hierarchical category view

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,26 +1,46 @@
+import { Category } from "@prisma/client";
 import { prisma } from "../../../prisma/client";
 import { createCategory, deleteCategory } from "../../actions/categories";
+import { relationalObjectsToHierarchy } from "@/utility";
 
 export default async function Categories() {
   const categories = await prisma.category.findMany();
+  const hierarchy = relationalObjectsToHierarchy(categories, (category) => ({
+    ...category,
+    parentId: category.parentCategoryId,
+  }));
 
   return (
     <>
       <h1>Categories</h1>
-      <ul>
-        {categories.map((category) => (
-          <div key={category.id}>
-            <li>{category.name}</li>
-            <DeleteCategoryButton id={category.id} />
-          </div>
-        ))}
-      </ul>
+      <CategoryHierarchy children={hierarchy} />
       <form action={createCategory}>
         <label htmlFor="name">Name</label>
         <input type="text" name="name" id="name" />
         <button type="submit">Submit</button>
       </form>
     </>
+  );
+}
+
+type CategoryWithChildren = Category & { children?: CategoryWithChildren[] };
+type CategoryHierarchyProps = {
+  children?: CategoryWithChildren[];
+};
+function CategoryHierarchy({ children }: CategoryHierarchyProps) {
+  return (
+    <ul>
+      {children &&
+        children.map((child) => (
+          <>
+            <li>
+              {child.name}
+              <DeleteCategoryButton id={child.id} />
+            </li>
+            {child.children && <CategoryHierarchy children={child.children} />}
+          </>
+        ))}
+    </ul>
   );
 }
 
---
+++ src/utility.ts
@@ -0,0 +1,29 @@
+type RelationalObject = { id: number; parentId: number | null };
+export function relationalObjectsToHierarchy<T>(
+  objects: T[],
+  relationalTransform: (obj: T) => T & RelationalObject
+): (T & { children?: T[] })[] {
+  const hierarchy: (T & { children?: T[] })[] = [];
+  const objDict: { [key: number]: T & RelationalObject & { children?: T[] } } =
+    {};
+  const transformed = objects.map((obj) => relationalTransform(obj));
+
+  for (const obj of transformed) {
+    objDict[obj.id] = obj;
+  }
+  for (const obj of transformed) {
+    if (obj.parentId === null) {
+      hierarchy.push(obj);
+      continue;
+    }
+    const parent = objDict[obj.parentId];
+    if (!parent)
+      throw new Error(
+        `Failed to find parent for this object ${JSON.stringify(obj)}`
+      );
+    if (parent.children === undefined) parent.children = [];
+    parent.children.push(obj);
+  }
+
+  return hierarchy;
+}
